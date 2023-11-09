03e09cbeba67d7da6731c2e3927443608dbb3788
이호연 <ho991217@kakao.com>
2023-11-09 18:59:45 +0900

    feat: 사용자로부터 방문 예정 날짜 입력

--- src/InputView.js
+++ src/InputView.js
@@ -1,10 +1,9 @@
 import { Console } from '@woowacourse/mission-utils';
+import PROMPT from './constants/prompt';
 
 const InputView = {
   async readDate() {
-    const input = await Console.readLineAsync(
-      '12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)'
-    );
+    const input = await Console.readLineAsync(PROMPT.ASK_DATE);
     // ...
   },
   // ...
---
+++ src/constants/prompt.js
@@ -0,0 +1,7 @@
+const ASK_DATE = '12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)';
+
+const PROMPT = Object.freeze({
+  ASK_DATE,
+});
+
+export default PROMPT;
