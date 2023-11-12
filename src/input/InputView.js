f084c9b8af686678d2f4d4126618cb06c0a216f6
이호연 <ho991217@kakao.com>
2023-11-12 17:33:24 +0900

    feat: 식당 에상 방문 날짜 출력 prompt 추가

--- src/input/InputView.js
+++ src/input/InputView.js
@@ -1,12 +1,12 @@
 import { Console } from '@woowacourse/mission-utils';
-import PROMPT from './constants/prompt';
+import PROMPT from '../constants/prompt';
 
 const InputView = {
   async readDate() {
     const input = await Console.readLineAsync(PROMPT.ASK_DATE);
-    // ...
+
+    return input;
   },
-  // ...
 };
 
 export default InputView;
