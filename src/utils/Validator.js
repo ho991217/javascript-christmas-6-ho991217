607e86d2200d4cd318e98cd252fb88903d2751cd
이호연 <ho991217@kakao.com>
2023-11-15 18:20:00 +0900

    docs: readme 체크리스트 체크

--- src/utils/Validator.js
+++ src/utils/Validator.js
@@ -4,18 +4,6 @@ import { REGEX } from '../constants/index.js';
  * @typedef {import('../constants/number.js').Range} Range
  */
 
-/**
- * @typedef {Object} Validator
- * @property {number} expectVisitingDate 방문 예정일
- * @property {function(string): boolean} isPositiveInteger 양의 정수인지 확인하는 메소드
- * @property {function(string): boolean} isEmpty 빈 문자열인지 확인하는 메소드
- * @property {function(string, Range): boolean} isInRange 범위 내의 정수인지 확인하는 메소드
- * @property {function(string): boolean} isTokenizable 토큰화 가능한 문자열인지 확인하는 메소드
- */
-
-/**
- * @type {Validator}
- */
 const Validator = {
   isPositiveInteger(value) {
     return REGEX.POSITIVE_INTEGER.test(value);
